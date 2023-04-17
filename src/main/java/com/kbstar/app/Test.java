4861fbd48eb2648f61983953bb93eb46dab3b3d1
jeanman.lee@gmail.com <jeanman.lee@gmail.com>
2023-04-17 17:05:56 +0900

    0417

---
+++ src/main/java/com/kbstar/app/Test.java
@@ -0,0 +1,9 @@
+package com.kbstar.app;
+
+public class Test {
+    public static void main(String args[]){
+        TestService service = new TestService();
+        service.register();
+    }
+
+}
